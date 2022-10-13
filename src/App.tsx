5fa020ab9dd0e60856231bf451b8decfd6f6fe83
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-10-13 17:45:26 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/global.css'
+export function App() {
+ 
+
+  return (
+
+    <>
+    <h1 className="font-bold text-2xl text-gray-400 ">Hello Word</h1>
+
+      <button className='bg-cyan-500'>Enviar</button>
+    </>
+  )
+}
+
+
